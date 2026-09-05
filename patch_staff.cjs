cbcb26ed20c61bb90ac266b5b5a8ab1bdddab293
Randy <randythurion@gmail.com>
2026-09-05 10:34:19 +0800

    chore: remove scratch scripts

--- patch_staff.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const file = 'src/staff/StaffPortal.jsx';
-let content = fs.readFileSync(file, 'utf8');
-
-// We need to fetch settings first in submit() or earlier.
-// Does StaffPortal load settings?
